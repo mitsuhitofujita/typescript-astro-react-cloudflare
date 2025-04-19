6d3b1567e8a0c0a55f6c8ca1ab8f50be99454c2a
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-04-19 16:15:57 +0000

    feat: add react

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  })
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
+	integrations: [react()],
 });
